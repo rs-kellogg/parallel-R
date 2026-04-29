15bf3629d57f5732bcf364136d4504345851deb4
ambreenchaudhri <49079761+ambreenchaudhri@users.noreply.github.com>
2026-04-29 11:31:59 -0500

    Silently load libraries and improve comments

--- time_trial.R
+++ time_trial.R
@@ -2,17 +2,19 @@
 # Parallelize R Code
 ####################
 
-# libraries
-library(tidyverse)
-library(furrr)
-library(tictoc)
+# load libraries silently
+invisible(capture.output(suppressMessages({
+  library(tidyverse)
+  library(furrr)
+  library(tictoc)
+})))
 
-
-plan(multicore, workers = 1) #only hire one core for the job
+# ---- parallelization demo -----
+plan(multicore, workers = 1) # only hire one core for the job
 tic()
 
 seq(10) %>% #do 10 jobs
 
-future_walk(~Sys.sleep(5)) #each job tells computer to sleep for 5 seconds
+future_walk(~Sys.sleep(5)) # each job tells computer to sleep for 5 seconds
 
 toc() %>% write_rds("time_results.rds")
